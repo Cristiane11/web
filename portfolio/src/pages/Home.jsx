6293324c7112aa0a107aa8315a55f07f60a8211c
Cristiane11 <bjwmcd@yahoo.com>
2025-07-02 15:54:40 -0400

    update

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -2,10 +2,17 @@
 
 function Home() {
   return (
-    <div className="#">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a basic React 19.1.0 + Vite setup.</p>
-    </div>
+     <section className="home">
+      <img src='../assets/img/cris.jpeg' alt="Cristiane Abreu" className="home-image" />
+      <div className="home-content">
+        <h1>Hi, I'm Cristiane Abreu.</h1>
+        <p>
+          I’m a front-end developer and SEO consultant. I build clean, performant, and accessible websites
+          using modern web technologies like React, JavaScript, and Node.js. With nearly a decade of experience,
+          I help businesses grow their online presence through strategic SEO and effective UI development.
+        </p>
+      </div>
+    </section>
   );
 }
 
