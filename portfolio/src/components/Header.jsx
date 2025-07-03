bae083e94fcd6d6294f85a00215f242f54087983
Cristiane11 <bjwmcd@yahoo.com>
2025-07-03 14:04:29 -0400

    update

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import {NavLink } from "react-router-dom";
-import { FaGithub, FaLinkedin, FaFacebook, FaEnvelope } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaFacebook, FaEnvelope,FaGoogle} from "react-icons/fa";
 
 import '../index.css';
 
@@ -7,17 +7,10 @@ function Header() {
   
   return (
     <>
-     {/* 🔝 Top social bar */}
-      <div className="top-bar">
-        <div className="social-icons">
-          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-          <a href="https://facebook.com/yourprofile" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
-          <a href="mailto:youremail@example.com"><FaEnvelope /></a>
-        </div>
-      </div>
+    
     <header className="header">
       <h1 className="logo">CristianeAbreu</h1>
+
     <nav className="nav">
       <NavLink to="/" activeClassName="active">Home</NavLink>
       <NavLink to="/education" activeClassName="active">Education</NavLink>
@@ -26,6 +19,16 @@ function Header() {
       <NavLink to="/about" activeClassName="active">About</NavLink>
       <NavLink to="/contact" activeClassName="active">Contact</NavLink>
     </nav>
+   
+      <div className="top-bar">
+        <div className="social-icons">
+          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+          <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          <a href="https://facebook.com/yourprofile" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+          <a href="https://google.com/yourprofile" target="_blank" rel="noopener noreferrer"><FaGoogle /></a>
+          <a href="mailto:youremail@example.com"><FaEnvelope /></a>
+        </div>
+      </div>
     </header>
     </>
   );
