685d1d3d82415f2684467b6be0b1adca49ca288c
Cristiane11 <bjwmcd@yahoo.com>
2025-07-17 19:47:18 -0400

    Update

--- portfolio/src/data/projectData.js
+++ portfolio/src/data/projectData.js
@@ -4,32 +4,32 @@ const projects = [
     img: "./src/assets/img/Athens1.jpg",
     title: " Real estate WordPress Website",
     projectSkills: "JS, HTML, CSS, React",
-    description: "E-commerce WordPress site for fashion brand.",
+    description: "E-commerce WordPress site for a local Real Estate Agent.",
     link: "https://athensrealestate.pro/"
   },
   {
     id: 2,
     img: "./src/assets/img/website.jpg",
     title: "SEO Marketing Agency",
-    projectSkills: "JS, HTML, CSS, React",
+    projectSkills: "JS, HTML, CSS, php",
     description: "SEO marketing agency website with custom design and functionality.",
     link: "https://professionalwebsite.co/ "
   },
   {
     id: 3,
-    img: "./src/assets/img/SEOforWordpress.jpeg",
-    title: "Procurement Portal",
-    projectSkills: "Node.js, MongoDB, React",
+    img: "./src/assets/img/customHair.jpg",
+    title: "Hair Extension Salons NYC",
+    projectSkills: "javaScript,HTML,css,sql",
     description: "Internal procurement tool with dashboard and reporting.",
-    link: "./contact"
+    link: "https://customhairbycatherine.com/"
   },
   {
     id: 4,
-    img: "./src/assets/img/website analysis.jpeg",
-    title: "Corporate Website",
-    projectSkills: "HTML, CSS, JS",
+    img: "./src/assets/img/fashionClub.jpg",
+    title: "Fashion Club",
+    projectSkills: "HTML, CSS, JS, PHP, MYSQL,SEO",
     description: "Company portfolio built with vanilla stack.",
-     link: "./contact"
+     link: "https://fashionclubnj.com/"
   }
 ];
 export default projects;
