47c333a5e7b0df44a9dd12cb4bd27df2da031a0c
Cristiane11 <bjwmcd@yahoo.com>
2025-07-18 13:46:54 -0400

    update

--- portfolio/src/data/projectData.js
+++ portfolio/src/data/projectData.js
@@ -3,24 +3,24 @@ const projects = [
     id: 1,
     img: "./src/assets/img/Athens1.jpg",
     title: " Real estate WordPress Website",
-    projectSkills: "JS, HTML, CSS, React",
-    description: "E-commerce WordPress site for a local Real Estate Agent.",
+    projectSkills: "JavaScript, HTML, CSS, MYSQL",
+    description: "Designed a clean, mobile-friendly WordPress website for a local real estate agent to showcase listings and attract clients.",
     link: "https://athensrealestate.pro/"
   },
   {
     id: 2,
     img: "./src/assets/img/website.jpg",
     title: "SEO Marketing Agency",
-    projectSkills: "JS, HTML, CSS, php",
-    description: "SEO marketing agency website with custom design and functionality.",
+    projectSkills: "JavaScript, HTML, CSS, PHP",
+    description: "WordPress website for a freelance digital SEO marketing project, designed to boost lead generation and local visibility",
     link: "https://professionalwebsite.co/ "
   },
   {
     id: 3,
     img: "./src/assets/img/customHair.jpg",
     title: "Hair Extension Salons NYC",
-    projectSkills: "javaScript,HTML,css,sql",
-    description: "Internal procurement tool with dashboard and reporting.",
+    projectSkills: "JavaScript, HTML, CSS, SQL",
+    description: "Redesigned the Hair Extension Salons NYC website to boost lead generation and improve local visibility",
     link: "https://customhairbycatherine.com/"
   },
   {
@@ -28,7 +28,7 @@ const projects = [
     img: "./src/assets/img/fashionClub.jpg",
     title: "Fashion Club",
     projectSkills: "HTML, CSS, JS, PHP, MYSQL,SEO",
-    description: "Company portfolio built with vanilla stack.",
+    description: "Created an eCommerce website for Fashion Club to sell luxury dresses and enhance the brand’s online presence.",
      link: "https://fashionclubnj.com/"
   }
 ];
